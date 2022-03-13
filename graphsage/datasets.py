a525b4902ff3c8bd716473bb70775be822e567e3
Wilfried Bounsi <wilcoln99@gmail.com>
2022-03-13 15:57:38 +0000

    add graphsage core components, sage layer and uniform sampler

--- graphsage/datasets.py
+++
@@ -1,3 +0,0 @@
-import torch
-import torch.utils.data
-"""Here we implement all the datasets"""
---
+++ graphsage/settings.py
@@ -0,0 +1,3 @@
+import os.path as osp
+
+DATA_DIR = osp.join(osp.dirname(osp.realpath(__file__)), '..', 'data')
