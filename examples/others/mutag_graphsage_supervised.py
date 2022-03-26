2debda8ec649c831d81d8fbdcb228e5f440c9bc6
Hunar Batra <i@hunarbatra.com>
2022-03-26 22:17:31 +0530

    added mutag graphsage supervised

---
+++ examples/others/mutag_graphsage_supervised.py
@@ -0,0 +1,76 @@
+import torch
+from torch_geometric.datasets import TUDataset
+
+from torch_geometric.loader import DataLoader
+from torch.nn import Linear
+import torch.nn.functional as F
+from torch_geometric.nn import global_mean_pool
+
+from graphsage import settings
+from graphsage.layers import SAGE
+
+dataset = TUDataset(root='data/TUDataset', name='MUTAG')
+dataset = dataset.shuffle()
+train_dataset = dataset[:150]
+test_dataset = dataset[150:]
+
+train_loader = DataLoader(train_dataset, batch_size=64, shuffle=True)
+test_loader = DataLoader(test_dataset, batch_size=64, shuffle=False)
+
+
+class GraphSAGE(torch.nn.Module):
+    def __init__(self, hidden_channels):
+        super(GraphSAGE, self).__init__()
+        torch.manual_seed(12345)
+        self.conv1 = SAGE(dataset.num_node_features, hidden_channels)
+        self.conv2 = SAGE(hidden_channels, hidden_channels)
+        self.conv3 = SAGE(hidden_channels, hidden_channels)
+        self.lin = Linear(hidden_channels, dataset.num_classes)
+
+    def forward(self, x, edge_index, batch):
+        # obtain node embeddings 
+        x = self.conv1(x, edge_index)
+        x = x.relu()
+        x = self.conv2(x, edge_index)
+        x = x.relu()
+        x = self.conv3(x, edge_index)
+
+        # readout layer
+        x = global_mean_pool(x, batch)  # [batch_size, hidden_channels]
+
+        # final classifier
+        x = F.dropout(x, p=0.5, training=self.training)
+        x = self.lin(x)
+        
+        return x
+
+model = GraphSAGE(hidden_channels=64)
+optimizer = torch.optim.Adam(model.parameters(), lr=0.01)
+criterion = torch.nn.CrossEntropyLoss()
+
+def train():
+    model.train()
+
+    for data in train_loader:  # Iterate in batches over the training dataset.
+         out = model(data.x, data.edge_index, data.batch)  # Perform a single forward pass.
+         loss = criterion(out, data.y)  # Compute the loss.
+         loss.backward()  # Derive gradients.
+         optimizer.step()  # Update parameters based on gradients.
+         optimizer.zero_grad()  # Clear gradients.
+
+def test(loader):
+     model.eval()
+
+     correct = 0
+     for data in loader:  # Iterate in batches over the training/test dataset.
+         out = model(data.x, data.edge_index, data.batch)  
+         pred = out.argmax(dim=1)  # Use the class with highest probability.
+         correct += int((pred == data.y).sum())  # Check against ground-truth labels.
+     return correct / len(loader.dataset)  # Derive ratio of correct predictions.
+
+
+for epoch in range(1, 11):
+    train()
+    train_acc = test(train_loader)
+    test_acc = test(test_loader)
+    print(f'Epoch: {epoch:03d}, Train Acc: {train_acc:.4f}, Test Acc: {test_acc:.4f}')
