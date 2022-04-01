e86c99a3f43875dfce5636efde452bd9585e1c9c
Wilfried Bounsi <wilcoln99@gmail.com>
2022-04-01 06:37:24 +0100

    add script to run experiments

---
+++ scripts/run_experiments.sh
@@ -0,0 +1,3 @@
+python experiments/table1/run.py --batch_size=512 --num_epochs=10
+python experiments/fig2a/run.py --batch_size=512 --num_epochs=10
+python experiments/fig2b/run.py --batch_size=512 --num_epochs=10
