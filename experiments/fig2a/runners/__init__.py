42197a6ae81733328d998be1cc0b2729b9be993e
Wilfried Bounsi <wilcoln99@gmail.com>
2022-04-01 06:22:30 +0100

    update runners for fig2a and fig2b

--- experiments/fig2a/runners/__init__.py
+++ experiments/fig2a/runners/__init__.py
@@ -1,18 +1,10 @@
 from experiments.fig2a.runners import graphsage_runners
 
-# Dict of all implemented runners
-runners = {
-    # model: runner
-    'graphsage_mean': graphsage_runners.get('mean'),
-    'graphsage_max': graphsage_runners.get('max'),
-    'graphsage_gcn': graphsage_runners.get('gcn'),
-    'graphsage_lstm': graphsage_runners.get('lstm'),
-}
-
 
 def get(model):
     """Returns the runner for the given dataset, model and training mode."""
     try:
-        return runners[model]
+        aggregator = model.split('_')[1]
+        return graphsage_runners.get(aggregator)
     except KeyError:
         raise NotImplementedError
--- experiments/fig2b/runners/__init__.py
+++ experiments/fig2b/runners/__init__.py
@@ -1,13 +1,9 @@
 from .runners import SampleSizeRunner
-# Dict of all implemented runners
-from ..settings import SAMPLE_SIZES
-
-runners = {sample_size: SampleSizeRunner(sample_size) for sample_size in SAMPLE_SIZES}
 
 
 def get(sample_size):
     """Returns the runner for the given dataset, model and training mode."""
     try:
-        return runners[sample_size]
+        return SampleSizeRunner(sample_size)
     except KeyError:
         raise NotImplementedError
