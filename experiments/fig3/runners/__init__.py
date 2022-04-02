94e94e66c568be2513d5fb214f4fa20bdf35ad23
Wilfried Bounsi <wilcoln99@gmail.com>
2022-04-02 20:44:06 +0100

    add runner for raw_features model to fig3 experiment

--- experiments/fig3/runners/__init__.py
+++ experiments/fig3/runners/__init__.py
@@ -1,4 +1,4 @@
-from experiments.fig3.runners import graphsage_runners
+from experiments.fig3.runners import graphsage_runners, raw_features_runner
 
 
 def get(model, noise_prop):
@@ -6,5 +6,8 @@ def get(model, noise_prop):
     if 'graphsage' in model:
         aggregator = model.split('_')[1]
         return graphsage_runners.get(aggregator, noise_prop)
+
+    if model == 'raw_features':
+        return raw_features_runner.get(noise_prop)
     else:
         raise NotImplementedError
