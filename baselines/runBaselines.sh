606fa0952dacc3be2ff24b10533876878d107a50
Sami Labbaki <sami.labbaki@keble.ox.ac.uk>
2022-04-10 23:27:02 +0100

    add Flickr rawFeatures

--- baselines/runBaselines.sh
+++ baselines/runBaselines.sh
@@ -1,14 +1,15 @@
-python3 randomClassifierReddit.py
-python3 randomClassifierPPI.py
-python3 randomClassifierCora.py
-python3 randomClassifierCiteseer.py
-python3 randomClassifierPubmed.py
-python3 rawFeaturesReddit.py
-python3 rawFeaturesPPI.py
-python3 rawFeaturesCora.py
-python3 rawFeaturesCiteseer.py
-python3 rawFeaturesPubmed.py
-python3 deepWalkReddit.py
-python3 deepWalkCora.py
-python3 deepWalkCiteseer.py
-python3 deepWalkPubmed.py
+# python3 randomClassifierReddit.py
+# python3 randomClassifierPPI.py
+# python3 randomClassifierCora.py
+# python3 randomClassifierCiteseer.py
+# python3 randomClassifierPubmed.py
+# python3 rawFeaturesReddit.py
+# python3 rawFeaturesPPI.py
+# python3 rawFeaturesCora.py
+# python3 rawFeaturesCiteseer.py
+# python3 rawFeaturesPubmed.py
+python3 rawFeaturesFlickr.py
+# python3 deepWalkReddit.py
+# python3 deepWalkCora.py
+# python3 deepWalkCiteseer.py
+# python3 deepWalkPubmed.py
